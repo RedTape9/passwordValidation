d61a7c13b098c97f3f66af988f035371a64442be
Sergej Jaudszims <redtape9688@gmail.com>
2023-11-06 11:10:39 +0100

    edit pom.xml for CI

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,71 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String pwd = "superSafePW";
+    }
+
+    public static boolean isPWLengthGreaterOrEqualThan8(String pwd) {
+        return pwd.length() >= 8;
+    }
+
+    public static boolean isPWContainsAtLeastOneDigit(String pwd) {
+       // return pwd.matches(".*\\d.*");
+
+        /*String numbers = "0123456789";
+        for (int i = 0; i < pwd.length(); i++) {
+            if (numbers.contains(pwd.substring(i, i + 1))) {
+                return true;
+            }
+        } return false;*/
+
+
+        for (int i = 0; i < pwd.length(); i++) {
+            if (Character.isDigit(pwd.charAt(i))) {
+                return true;
+            }
+        } return false;
+
+    }
+
+    public static boolean isPWContainsAtLeastOneUpperAndOneLowerCaseLetter(String pwd) {
+        boolean isUpper = false;
+        boolean isLower = false;
+        for (int i = 0; i < pwd.length(); i++) {
+            if (Character.isLetter(pwd.charAt(i))) {
+                if (Character.isUpperCase(pwd.charAt(i))) {
+                    isUpper = true;
+                } else {
+                    isLower = true;
+                }
+            }
+        }
+        return isUpper && isLower;
+
+        /*
+        String regExp = "^(?=.*[a-z])(?=.*[A-Z]).*$";
+        return pwd.matches(regExp);
+        */
+    }
+
+    public static boolean isPWAWellKnownPW(String pwd) {
+        String[] wellKnownPWs = {
+                "PW1",
+                "Admin1234",
+                "Welcome1",
+                "Secret12",
+                "ChangeMe1",
+                "Superman1",
+                "Letmein12",
+                "Qwerty123",
+                "Trustno12",
+                "Access123"
+        };
+        for (int i = 0; i < wellKnownPWs.length; i++) {
+            if (pwd.equals(wellKnownPWs[i])) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,4 +1,41 @@
+package org.example;
+
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 class MainTest {
-  
+
+    @Test
+    void isPWLengthGreaterOrEqualThan8() {
+        assertEquals(true, Main.isPWLengthGreaterOrEqualThan8("12345678"));
+        assertEquals(true, Main.isPWLengthGreaterOrEqualThan8("123SafePW"));
+        assertEquals(true, Main.isPWLengthGreaterOrEqualThan8("Albion123"));
+    }
+
+    @Test
+    void isPWContainsAtLeastOneDigit() {
+        assertEquals(true, Main.isPWContainsAtLeastOneDigit("12345678"));
+        assertEquals(true, Main.isPWContainsAtLeastOneDigit("123SafePW"));
+        assertEquals(true, Main.isPWContainsAtLeastOneDigit("Albion123"));
+    }
+
+    @Test
+    void isPWContainsAtLeastOneUpperAndOneLowerCaseLetter() {
+        assertEquals(false, Main.isPWContainsAtLeastOneUpperAndOneLowerCaseLetter("12345678"));
+        assertEquals(true, Main.isPWContainsAtLeastOneUpperAndOneLowerCaseLetter("123SafePW"));
+        assertEquals(true, Main.isPWContainsAtLeastOneUpperAndOneLowerCaseLetter("Albion123"));
+    }
+
+    @Test
+    void isPWAWellKnownPW() {
+        assertEquals(true, Main.isPWAWellKnownPW("Admin1234"));
+        assertEquals(true, Main.isPWAWellKnownPW("Welcome1"));
+        assertEquals(true, Main.isPWAWellKnownPW("Secret12"));
+        assertEquals(true, Main.isPWAWellKnownPW("ChangeMe1"));
+        assertEquals(true, Main.isPWAWellKnownPW("Superman1"));
+        assertEquals(true, Main.isPWAWellKnownPW("Letmein12"));
+        assertEquals(true, Main.isPWAWellKnownPW("Qwerty123"));
+        assertEquals(true, Main.isPWAWellKnownPW("Trustno12"));
+        assertEquals(true, Main.isPWAWellKnownPW("Access123"));
+    }
 }
